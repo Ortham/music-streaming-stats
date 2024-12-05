35031b5d17b1a9fee8985bf7c45d2250ac24b12c
Oliver Hamlet <git@ortham.net>
2024-12-05 20:01:35 +0000

    Add artist CSV and time-based bucketed CSV outputs to Spotify script

--- spotify-stats.py
+++ spotify-stats.py
@@ -2,9 +2,12 @@
 
 import argparse
 import csv
+from datetime import datetime, timedelta, timezone
 import json
 import os
 import pprint
+
+bucket_duration_secs = 3600
 
 def parse_input_json(dir_path):
     streams = []
@@ -62,10 +65,12 @@ def get_albums(tracks_by_id):
                 'artist_name': track['artist_name'],
                 'tracks_play_count': 0,
                 'complete_play_count': 0,
+                'play_time_ms': 0,
                 'track_ids': set()
             }
 
         albums_by_name[album_name]['tracks_play_count'] += track['complete_play_count']
+        albums_by_name[album_name]['play_time_ms'] += track['play_time_ms']
         albums_by_name[album_name]['track_ids'].add(track['track_id'])
 
     # The complete play count is a bit misleading, as it assumes that I've listened to all the tracks in the album at least once.
@@ -79,6 +84,121 @@ def get_albums(tracks_by_id):
     albums.sort(key=lambda a: a['tracks_play_count'], reverse=True)
 
     return albums
+
+def get_artists(tracks_by_id):
+    artists_by_name = {}
+    for track in tracks_by_id.values():
+        artist_name = track['artist_name']
+        if artist_name not in artists_by_name:
+            artists_by_name[artist_name] = {
+                'artist_name': artist_name,
+                'play_count': 0,
+                'complete_play_count': 0,
+                'play_time_ms': 0,
+                'track_ids': set()
+            }
+
+        artists_by_name[artist_name]['play_count'] += track['play_count']
+        artists_by_name[artist_name]['complete_play_count'] += track['complete_play_count']
+        artists_by_name[artist_name]['play_time_ms'] += track['play_time_ms']
+        artists_by_name[artist_name]['track_ids'].add(track['track_id'])
+
+    artists = list(artists_by_name.values())
+    artists.sort(key=lambda a: a['play_count'], reverse=True)
+
+    return artists
+
+
+def get_day_of_week(timestamp: datetime):
+    week_days = {
+        1: 'Mon',
+        2: 'Tue',
+        3: 'Wed',
+        4: 'Thu',
+        5: 'Fri',
+        6: 'Sat',
+        7: 'Sun'
+    }
+    return week_days[timestamp.isoweekday()]
+
+def get_bucket_start_timestamp(timestamp: datetime):
+    return datetime.fromtimestamp((timestamp.timestamp() // bucket_duration_secs) * bucket_duration_secs, tz=timezone.utc)
+
+def get_hour_bucket_key(timestamp: datetime):
+    return get_bucket_start_timestamp(timestamp).isoformat()
+
+def get_hour_of_week_bucket_key(timestamp: datetime):
+    day_of_week = get_day_of_week(timestamp)
+
+    return f'{timestamp.isoweekday()} {day_of_week} {timestamp.hour:02}:00'
+
+def get_buckets(streams, get_bucket_key):
+    streams.sort(key=lambda s: s['ts'])
+
+    buckets_by_key = {}
+    for stream in streams:
+        if stream['spotify_track_uri'] is None:
+            # It's probably a podcast episode
+            continue
+
+        # The timestamp is when the track stopped playing, but it may have been playing across multiple buckets, and should be partially recorded against each relevant bucket to avoid over-representing playtime in any one bucket.
+        end_timestamp = datetime.fromisoformat(stream['ts'])
+        start_timestamp = end_timestamp - timedelta(milliseconds=stream['ms_played'])
+
+        keys = {}
+        current_ts = start_timestamp
+        key = get_bucket_key(current_ts)
+        end_key = get_bucket_key(end_timestamp)
+        while key != end_key:
+            bucket_time_remaining = timedelta(seconds=bucket_duration_secs) - (current_ts - get_bucket_start_timestamp(current_ts))
+
+            keys[key] = bucket_time_remaining / timedelta(milliseconds=1)
+
+            current_ts += bucket_time_remaining
+            key = get_bucket_key(current_ts)
+
+        keys[key] = (end_timestamp - current_ts) / timedelta(milliseconds=1)
+
+        for (key, bucket_play_time_ms) in keys.items():
+            if key not in buckets_by_key:
+                buckets_by_key[key] = {
+                    'bucket_start': key,
+                    'play_time_ms': 0,
+                    'streams': []
+                }
+
+            buckets_by_key[key]['play_time_ms'] += bucket_play_time_ms
+
+            buckets_by_key[key]['streams'].append(stream)
+            buckets_by_key[key]['streams'][-1]['start_ts'] = start_timestamp.isoformat()
+
+    buckets = list(buckets_by_key.values())
+    buckets.sort(key=lambda b: b['play_time_ms'], reverse=True)
+
+    if get_bucket_key == get_hour_bucket_key:
+        # Some buckets may overflow due to bad timestamps and/or playtimes. There can be many tracks with the same end timestamp (and which were played for at least one second), and even when they're not the same, the end time of one stream may be after the (end time - play duration) of the next.
+        # Identical timestamps seem to often (always?) be offline = true, but the offline_timestamp field is not reliable (some streams have it set to times in 1970).
+        # This block checks that all the overflowed time is accounted for by overlaps between consecutive streams.
+        bucket_duration_ms = bucket_duration_secs * 1000
+        for bucket in buckets:
+            if bucket['play_time_ms'] <= bucket_duration_ms:
+                continue
+
+            overlap = timedelta(milliseconds=0)
+            for i in reversed(range(1, len(bucket['streams']))):
+                start_ts = datetime.fromisoformat(bucket['streams'][i]['start_ts'])
+                previous_end_ts = datetime.fromisoformat(bucket['streams'][i - 1]['ts'])
+
+
+                if previous_end_ts > start_ts:
+                    overlap += previous_end_ts - start_ts
+
+            overflow_due_to_overlap = bucket_duration_ms + (overlap / timedelta(milliseconds=1))
+            unexplained_overflow = bucket['play_time_ms'] - overflow_due_to_overlap
+            if unexplained_overflow > 0:
+                print(f'The bucket {bucket['bucket_start']} overflows by more than the overlap between streams, leaving {unexplained_overflow} ms unexplained')
+
+    return buckets
 
 def increment_count(counters, key):
     if key not in counters:
@@ -134,9 +254,9 @@ def get_platforms(stats):
 
     return platforms
 
-def write_stats_json(output_path, stats):
+def write_json(output_path, data):
     with open(output_path, 'w', encoding='utf-8') as f:
-        json.dump(stats, f, indent="\t")
+        json.dump(data, f, indent="\t")
 
 def write_csv(output_path, rows):
     with open(output_path, 'w', newline='', encoding='utf-8') as csv_file:
@@ -148,15 +268,34 @@ def write_csv(output_path, rows):
 
 def write_albums_csv(output_path, albums):
     with open(output_path, 'w', newline='', encoding='utf-8') as csv_file:
-        field_names = ['album_name','artist_name', 'tracks_play_count', 'complete_play_count']
+        field_names = ['album_name','artist_name', 'tracks_play_count', 'complete_play_count', 'play_time_ms']
         writer = csv.DictWriter(csv_file, fieldnames=field_names, extrasaction='ignore')
 
         writer.writeheader()
         for album in albums:
             writer.writerow(album)
 
+def write_artists_csv(output_path, artists):
+    with open(output_path, 'w', newline='', encoding='utf-8') as csv_file:
+        field_names = ['artist_name', 'play_count', 'complete_play_count', 'play_time_ms']
+        writer = csv.DictWriter(csv_file, fieldnames=field_names, extrasaction='ignore')
+
+        writer.writeheader()
+        for artist in artists:
+            writer.writerow(artist)
+
+def write_buckets_csv(output_path, buckets):
+    with open(output_path, 'w', newline='', encoding='utf-8') as csv_file:
+        field_names = ['bucket_start', 'play_time_ms']
+        writer = csv.DictWriter(csv_file, fieldnames=field_names, extrasaction='ignore')
+
+        writer.writeheader()
+        for bucket in buckets:
+            writer.writerow(bucket)
+
 def main():
     parser = argparse.ArgumentParser(description='Supply the path to a directory of JSON files containing your Spotify extended streaming history.')
+    parser.add_argument('-b', '--bucket-type', choices=['hour', 'hour-of-week'], default='hour')
     parser.add_argument('input_dir_path')
     parser.add_argument('output_dir_path')
     args = parser.parse_args()
@@ -166,12 +305,18 @@ def main():
     tracks_by_id = get_tracks(streams)
 
     albums = get_albums(tracks_by_id)
+    artists = get_artists(tracks_by_id)
+
+    get_bucket_key = get_hour_of_week_bucket_key if args.bucket_type == 'hour-of-week' else get_hour_bucket_key
+    buckets = get_buckets(streams, get_bucket_key)
 
     stats = collect_stats(streams, len(tracks_by_id))
     ip_addrs = get_ip_addrs(stats)
     platforms = get_platforms(stats)
 
-    write_stats_json(os.path.join(args.output_dir_path, 'spotify_stats.json'), stats)
+    write_json(os.path.join(args.output_dir_path, 'spotify_streams.json'), streams)
+
+    write_json(os.path.join(args.output_dir_path, 'spotify_stats.json'), stats)
 
     write_csv(os.path.join(args.output_dir_path, 'spotify_ip_addrs.csv'), ip_addrs)
 
@@ -180,6 +325,10 @@ def main():
     write_csv(os.path.join(args.output_dir_path, 'spotify_tracks.csv'), tracks_by_id.values())
 
     write_albums_csv(os.path.join(args.output_dir_path, 'spotify_albums.csv'), albums)
+
+    write_artists_csv(os.path.join(args.output_dir_path, 'spotify_artists.csv'), artists)
+
+    write_buckets_csv(os.path.join(args.output_dir_path, 'spotify_times.csv'), buckets)
 
 if __name__ == "__main__":
     main()
