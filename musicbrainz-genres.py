c9ccaec2b103731cd5513c42e3c0960774c0076a
Oliver Hamlet <git@ortham.net>
2024-12-23 16:06:59 +0000

    Update genres script to always dump all tags

--- musicbrainz-genres.py
+++ musicbrainz-genres.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+from pprint import pp
 
 from helpers import read_json, write_json, connect_to_postgres
 
@@ -20,28 +21,10 @@ def get_recording_tags(postgres_connection, musicbrainz_recording_id):
 
         return rows
 
-def get_recording_genres(postgres_connection, musicbrainz_recording_id):
-    # Not all tags are genres that are recognised by MusicBrainz, this only
-    # returns tags that are genres
-    with postgres_connection.cursor() as cur:
-        cur.execute("""
-                    select tag.name, rt.count, genre.gid as genre_id
-                        from recording r
-                        join recording_tag rt on r.id = rt.recording
-                        join tag on rt.tag = tag.id
-                        join genre on genre.name = tag.name
-                        where r.gid = %s
-                    """, (musicbrainz_recording_id, ))
-        rows = cur.fetchall()
-
-        return rows
-
 def main():
-    parser = argparse.ArgumentParser(description='Supply the path to a JSON file containing $.mbids_by_isrc')
+    parser = argparse.ArgumentParser(description='Supply the path to a JSON file containing $.mbids_by_spotify_uri')
     parser.add_argument('--input-path')
     parser.add_argument('--output-path')
-    parser.add_argument('--get-all-tags', action='store_const', const=True)
-    parser.add_argument('--postgresql', action='store_const', const=True)
     parser.add_argument('--postgresql-host', default='localhost')
     parser.add_argument('--postgresql-port', default='6543')
     parser.add_argument('--postgresql-sslmode', default='disable')
@@ -54,9 +37,6 @@ def main():
 
     mbids = set(mbid for mbids in mbids_by_spotify_uri.values() for mbid in mbids)
 
-    print('Tracks', len(mbids_by_spotify_uri))
-    print('MBIDs', len(mbids))
-
     postgres_connection = connect_to_postgres(args.postgresql_host,
                                                 args.postgresql_port,
                                                 args.postgresql_sslmode,
@@ -65,16 +45,23 @@ def main():
                                                 args.postgresql_password)
 
     recording_genres = {}
+    all_genres = []
+    other_tags = []
     with postgres_connection:
         for recording_id in mbids:
-            if args.get_all_tags:
-                genres = get_recording_genres(postgres_connection, recording_id)
-            else:
-                genres = get_recording_tags(postgres_connection, recording_id)
-            genres = [{'name': genre[0], 'tag_count': genre[1], 'genre_id': str(genre[2])} for genre in genres]
+            genres = get_recording_tags(postgres_connection, recording_id)
+            genres = [{'name': genre[0], 'tag_count': genre[1], 'genre_id': str(genre[2]) if genre[2] else None} for genre in genres]
 
             if genres:
                 recording_genres[recording_id] = genres
+
+                for genre in genres:
+                    if genre['genre_id']:
+                        all_genres.append(genre['name'])
+                    else:
+                        other_tags.append(genre['name'])
+
+    print(f'Mapped {len(recording_genres)} of {len(mbids)} recordings to {len(all_genres)} genres ({len(set(all_genres))} unique) and {len(other_tags)} other tags ({len(set(other_tags))} unique)')
 
     if args.output_path:
         write_json(args.output_path, recording_genres)
