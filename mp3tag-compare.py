bd1a84065b9911a017a5221161e8052c9a1a2013
Oliver Hamlet <git@ortham.net>
2024-12-27 11:36:38 +0000

    Update mp3tag-compare script
    To compare lookup results against the output of the acoustid-match script instead of scan results.

--- mp3tag-compare.py
+++ mp3tag-compare.py
@@ -4,7 +4,7 @@ import argparse
 import csv
 import os
 
-from pprint import pp
+from helpers import read_json
 
 def parse_input_csv(file_path):
     tracks = []
@@ -31,34 +31,39 @@ def process_rows(rows, file_path):
 def main():
     parser = argparse.ArgumentParser(description='Supply the path to a JSON file containing $[*].external_ids.isrc fields.')
     parser.add_argument('--mp3tag-lookup-export-path')
-    parser.add_argument('--mp3tag-scan-export-path')
+    parser.add_argument('--acoustid-matches-path')
+    parser.add_argument('--acoustid-matches-base-path')
     args = parser.parse_args()
 
     rows = parse_input_csv(args.mp3tag_lookup_export_path)
     lookup_data = process_rows(rows, args.mp3tag_lookup_export_path)
 
-    rows = parse_input_csv(args.mp3tag_scan_export_path)
-    scan_data = process_rows(rows, args.mp3tag_scan_export_path)
+    match_data = read_json(args.acoustid_matches_path)
+    matched_relative_paths = {}
+    for match in match_data['matches']:
+        rel_path = os.path.relpath(match['file_path'], start=args.acoustid_matches_base_path)
+        if rel_path in matched_relative_paths:
+            matched_relative_paths[rel_path].append(match)
+        else:
+            matched_relative_paths[rel_path] = [match]
 
-    mismatched_isrc_keys = []
-    mismatched_mbid_keys = []
+    unmatched_relative_paths = set(os.path.relpath(p, start=args.acoustid_matches_base_path) for p in match_data['unmatched_paths'])
 
-    for key in lookup_data:
-        lookup_entry = lookup_data[key]
-        scan_entry = scan_data[key]
+    for lookup_entry in lookup_data.values():
+        mbid = lookup_entry['MBID']
+        rel_path = lookup_entry['relative_path']
 
-        if lookup_entry['MBID'] != scan_entry['MBID']:
-            mismatched_mbid_keys.append(key)
+        if mbid:
+            if rel_path not in matched_relative_paths and rel_path not in unmatched_relative_paths:
+                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs or unmatched paths')
+            elif rel_path not in matched_relative_paths and rel_path:
+                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs')
 
-        if lookup_entry['ISRC'] != scan_entry['ISRC']:
-            mismatched_isrc_keys.append(key)
-
+            if rel_path in matched_relative_paths:
+                if mbid not in [m['recording_id'] for m in matched_relative_paths[rel_path]]:
+                    print(f'The AcoustID matches for {rel_path} do not include {mbid}')
 
     print(f'Processed {len(lookup_data)} tracks')
-    print('The following keys had different lookup and scan ISRCs:')
-    pp(mismatched_isrc_keys)
-    print('The following keys had different lookup and scan MBIDs:')
-    pp(mismatched_mbid_keys)
 
 if __name__ == "__main__":
     main()
