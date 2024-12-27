c2fa6611b95b3deb54441e48d77decad3f5f5e10
Oliver Hamlet <git@ortham.net>
2024-12-27 17:26:20 +0000

    Improve mp3tag-compare script
    Check the impact of MBID differences on genre and acoustic metadata.

--- mp3tag-compare.py
+++ mp3tag-compare.py
@@ -6,7 +6,7 @@ import os
 
 from helpers import read_json
 
-def parse_input_csv(file_path):
+def parse_input_tsv(file_path):
     tracks = []
     with open(file_path, encoding='utf-8-sig') as f:
         reader = csv.DictReader(f, delimiter='\t')
@@ -28,14 +28,33 @@ def process_rows(rows, file_path):
 
     return data_by_relative_path
 
+def get_lookup_only_tags(mbid, matches, musicbrainz_tags):
+    if mbid in musicbrainz_tags:
+        lookup_tags = set(g['name'] for g in musicbrainz_tags[mbid])
+    else:
+        lookup_tags = set()
+
+    acoustid_tags = set(g['name'] for m in matches if m['recording_id'] in musicbrainz_tags for g in musicbrainz_tags[m['recording_id']])
+
+    return lookup_tags - acoustid_tags
+
+def matches_have_acoustic_metadata(matches, acousticbrainz_metadata):
+    for match in matches:
+        if match['recording_id'] in acousticbrainz_metadata:
+            return True
+
+    return False
+
 def main():
     parser = argparse.ArgumentParser(description='Supply the path to a JSON file containing $[*].external_ids.isrc fields.')
     parser.add_argument('--mp3tag-lookup-export-path')
     parser.add_argument('--acoustid-matches-path')
     parser.add_argument('--acoustid-matches-base-path')
+    parser.add_argument('--musicbrainz-tags-path')
+    parser.add_argument('--acousticbrainz-path')
     args = parser.parse_args()
 
-    rows = parse_input_csv(args.mp3tag_lookup_export_path)
+    rows = parse_input_tsv(args.mp3tag_lookup_export_path)
     lookup_data = process_rows(rows, args.mp3tag_lookup_export_path)
 
     match_data = read_json(args.acoustid_matches_path)
@@ -49,19 +68,30 @@ def main():
 
     unmatched_relative_paths = set(os.path.relpath(p, start=args.acoustid_matches_base_path) for p in match_data['unmatched_paths'])
 
+    musicbrainz_tags = read_json(args.musicbrainz_tags_path)
+    acousticbrainz = read_json(args.acousticbrainz_path)
+
     for lookup_entry in lookup_data.values():
         mbid = lookup_entry['MBID']
         rel_path = lookup_entry['relative_path']
 
         if mbid:
-            if rel_path not in matched_relative_paths and rel_path not in unmatched_relative_paths:
-                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs or unmatched paths')
-            elif rel_path not in matched_relative_paths and rel_path:
-                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs')
-
             if rel_path in matched_relative_paths:
-                if mbid not in [m['recording_id'] for m in matched_relative_paths[rel_path]]:
+                matches = matched_relative_paths[rel_path]
+                if mbid not in [m['recording_id'] for m in matches]:
                     print(f'The AcoustID matches for {rel_path} do not include {mbid}')
+
+                    lookup_only_tags = get_lookup_only_tags(mbid, matches, musicbrainz_tags)
+                    if lookup_only_tags:
+                        print('The missing MBID leads to the following missing tags', lookup_only_tags)
+
+                    if mbid in acousticbrainz and not matches_have_acoustic_metadata(matches, acousticbrainz):
+                        print('The missing MBID leads to missing acoustic metadata')
+
+            elif rel_path in unmatched_relative_paths:
+                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs')
+            else:
+                print(f'Cannot find {mbid} for {rel_path} in matched MBIDs or unmatched paths')
 
     print(f'Processed {len(lookup_data)} tracks')
 
