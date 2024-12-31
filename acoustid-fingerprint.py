07dbcc9d331618a7d6f3a855369f834941c2d7e7
Oliver Hamlet <git@ortham.net>
2024-12-31 22:07:32 +0000

    Split acoustid-match script in two
    Calculate fingerprints separately from matching them, so that I can do the former offline and do the latter without access to the original files.

---
+++ acoustid-fingerprint.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+
+import argparse
+import os
+
+import acoustid
+
+from helpers import read_json, write_json
+
+audio_file_extensions = ['.mp3', '.flac', '.wav', '.m4a']
+other_file_extensions = ['.jpg', '.png', '.txt', '.pdf', '.log', '.rtf', '.exe', '.url', '.DS_Store']
+
+def ends_with_one_of(string, candidates):
+    for candidate in candidates:
+        if string.endswith(candidate):
+            return True
+
+    return False
+
+def extended_length_path(dos_path):
+    abs_path = os.path.abspath(dos_path)
+    if abs_path.startswith(u"\\\\"):
+        return u"\\\\?\\UNC\\" + abs_path[2:]
+
+    return u"\\\\?\\" + abs_path
+
+def find_audio_files(root_path):
+    audio_file_paths = []
+    for root, dirs, files in os.walk(root_path):
+        for file in files:
+            if ends_with_one_of(file, audio_file_extensions):
+                audio_file_paths.append(os.path.join(root, file))
+            elif not ends_with_one_of(file, other_file_extensions):
+                print('Unexpected file extension:', os.path.join(root, file))
+
+    return audio_file_paths
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--music-library-path')
+    parser.add_argument('--output-path')
+    args = parser.parse_args()
+
+    file_paths = find_audio_files(args.music_library_path)
+    print(f'Found {len(file_paths)} audio files')
+
+    output = {
+        'results': [],
+        'failed_paths': []
+    }
+    already_processed_paths = set()
+    try:
+        output = read_json(args.output_path)
+
+        for entry in output['results']:
+            already_processed_paths.add(entry['file_path'])
+    except Exception as e:
+        print(f'Could not read file at {args.output_path}, will fetch AcoustID matches for all audio files.')
+        pass
+
+    for file_path in file_paths:
+        if file_path in already_processed_paths:
+            continue
+
+        print(f'Attempting to calculate AcoustID fingerprint for "{file_path}"...')
+        try:
+            (duration, fingerprint) = acoustid.fingerprint_file(file_path)
+
+            output['results'].append({
+                'file_path': file_path,
+                'duration': duration,
+                'fingerprint': fingerprint.hex()
+            })
+        except Exception as e:
+            print(f'Failed to calculate AcoustID fingerprint for {file_path}', e)
+            output['failed_paths'].append(file_path)
+
+    write_json(args.output_path, output)
+
+if __name__ == "__main__":
+    main()
--- acoustid-match.py
+++ acoustid-match.py
@@ -1,49 +1,21 @@
 #!/usr/bin/env python3
 
 import argparse
-import os
 
 import acoustid
 
 from helpers import read_json, write_json
 
-audio_file_extensions = ['.mp3', '.flac', '.wav', '.m4a']
-other_file_extensions = ['.jpg', '.png', '.txt', '.pdf', '.log', '.rtf', '.exe', '.url', '.DS_Store']
-
-def ends_with_one_of(string, candidates):
-    for candidate in candidates:
-        if string.endswith(candidate):
-            return True
-
-    return False
-
-def extended_length_path(dos_path):
-    abs_path = os.path.abspath(dos_path)
-    if abs_path.startswith(u"\\\\"):
-        return u"\\\\?\\UNC\\" + abs_path[2:]
-
-    return u"\\\\?\\" + abs_path
-
-def find_audio_files(root_path):
-    audio_file_paths = []
-    for root, dirs, files in os.walk(root_path):
-        for file in files:
-            if ends_with_one_of(file, audio_file_extensions):
-                audio_file_paths.append(os.path.join(root, file))
-            elif not ends_with_one_of(file, other_file_extensions):
-                print('Unexpected file extension:', os.path.join(root, file))
-
-    return audio_file_paths
-
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--music-library-path')
+    parser.add_argument('--acoustid-fingerprints-path')
     parser.add_argument('--acoustid-api-key')
     parser.add_argument('--output-path')
     args = parser.parse_args()
 
-    file_paths = find_audio_files(args.music_library_path)
-    print(f'Found {len(file_paths)} audio files')
+    fingerprints = read_json(args.acoustid_fingerprints_path)
+    fingerprints = fingerprints['results']
+    print(f'Found {len(fingerprints)} AcoustID fingerprints')
 
     output = {
         'matches': [],
@@ -62,14 +34,17 @@ def main():
         print(f'Could not read file at {args.output_path}, will fetch AcoustID matches for all audio files.')
         pass
 
-    for file_path in file_paths:
+    for entry in fingerprints:
+        file_path = entry['file_path']
         if file_path in already_processed_paths:
             continue
 
         print(f'Attempting to match "{file_path}"...')
         found_match = False
         try:
-            results = acoustid.match(args.acoustid_api_key, file_path)
+            fingerprint = bytes.fromhex(entry['fingerprint'])
+            response = acoustid.lookup(args.acoustid_api_key, fingerprint, entry['duration'])
+            results = acoustid.parse_lookup_result(response)
 
             for score, recording_id, title, artist in results:
                 found_match = True
@@ -89,7 +64,7 @@ def main():
 
     write_json(args.output_path, output)
 
-    matched_file_count = len(file_paths) - len(output['unmatched_paths'])
+    matched_file_count = len(fingerprints) - len(output['unmatched_paths'])
     print(f'Found {len(output['matches'])} matches for {matched_file_count} files')
 
 if __name__ == "__main__":
